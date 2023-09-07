4ba532d6c72a2b9e73cb670de68d6b5ebd3ccab7
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-09-07 14:43:28 -0400

    routing

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
-})
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy una nueva ruta o endpont');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+
+});
+
 
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
-
-
