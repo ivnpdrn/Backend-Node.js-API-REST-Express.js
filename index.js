4268ec1fd0979d2c4df98dcb37fac5a4422c8bc6
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-09-08 15:11:25 -0400

    Query Params

--- index.js
+++ index.js
@@ -23,6 +23,19 @@ app.get('/products', (req, res) => {
   ]);
 });
 
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  }
+  else {
+    res.send('No hay parametros');
+  }
+});
+
 app.get('/products/:productId', (req, res) => {
   const { id } = req.params;
   res.json({
