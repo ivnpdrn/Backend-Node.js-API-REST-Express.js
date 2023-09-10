318c57c2fc3fe20ee9e7ebb56692caea9ebf2df0
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-09-09 21:36:30 -0400

    Index Original - Ya modificado - se le ha extraido

---
+++ index - Original.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const routerApi = require('./routes');
+// const {faker} = require('@faker-js/faker');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy una nueva ruta o endpont');
+});
+
+routerApi(app);
+
+
+//app.get('/users', (req, res) => {
+//  const { limit, offset } = req.query;
+//  if (limit && offset) {
+//    res.json({
+//      limit,
+//      offset
+//    });
+//  }
+//  else {
+//    res.send('No hay parametros');
+//  }
+//});
+
+
+//app.get('/categories/:categoryId/products/:productId', (req, res) => {
+//  const { categoryId, productId } = req.params;
+//  res.json({
+//    categoryId,
+//    productId,
+//   });
+//});
+    
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
