460c92641b41b731c6445b40a1ccab9080724034
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-09-16 22:29:37 -0400

    joi - validation

---
+++ product.schema.js
@@ -0,0 +1,30 @@
+// el proposito de este file es el de validar la data
+// que nos estan enviando desde el cliente: Angular o React
+
+const Joi = require('joi');
+
+// validar que formato debe tener cada campo
+
+const id = Joi.string().uuid();
+const name = Joi.string().alphanum().min(3).max(15);
+const price = Joi.number().integer().min(10);
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required(),
+});
+
+const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+});
+
+const getProductSchema = Joi.object({
+  id: id.required(),
+});
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema}
+
+
+
+
