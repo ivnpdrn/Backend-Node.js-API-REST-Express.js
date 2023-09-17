c7c5b4d173192e3a6ae818c1183902496d40819a
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-09-16 20:30:42 -0400

    boom - driving errors

--- error.handler.js
+++ error.handler.js
@@ -1,4 +1,4 @@
-function logErrors(err, req, res, next) {   // este middleware es para log errores
+function logErrors(err, req, res, next) {   
   console.log('logErrors');
   console.error(err);
   next(err);
@@ -10,11 +10,25 @@ function errorHandler(err, req, res, next) {    // crear un formato estandar cad
   console.log('errorHandler'); 
   res.status(500).json({
     message: err.message,
-    stack: err.stack,                  // para saber donde ocurrio el error
+    stack: err.stack,                  
   });
 }
 
-module.exports = { logErrors, errorHandler }
+
+function boomErrorHandler(err, req, res, next) {
+  if (err.isBoom) {                                      // si es del tipo BOOM aqui queda
+    const { output } = err;
+    res.status(output.statusCode).json(output.payload); // si el error es tipo BOOM corre esta linea,
+                      //ya modificamoscreando la respuesta al request, y aca deberia terminar,
+                      // por eso hasta aca deberia llegar entonces la necesidad del ELSE. 
+  } else {           // debe agregarse el ELSE si no se coloca siempre se ejecutara y dara el error:
+                    //  "Cannot set headers after they are sent to the clent"
+                    //  No se puede configurar el header dos veces.
+    next(err);     // si no es tipo Boom, va al siguiente MIDDLEWARES
+  }  
+}
+
+module.exports = { logErrors, errorHandler, boomErrorHandler }
 
 // los middlewares deben hacerse despues que se han definido los reuters
 
